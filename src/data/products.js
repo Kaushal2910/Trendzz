b7114eb3c47a7d0b0da0eef31cd39b87eaa9e32e
Kaushal2910 <your-email@example.com>
2025-07-29 22:23:02 +0530

    folder and files created

---
+++ src/data/products.js
@@ -0,0 +1,20 @@
+
+const products = [
+  {
+    id: 1,
+    name: "Black T-Shirt",
+    price: 499,
+    image: "/images/tshirt.jpg",
+    category: "men",
+  },
+  {
+    id: 2,
+    name: "Blue Dress",
+    price: 899,
+    image: "/images/dress.jpg",
+    category: "women",
+  },
+  // add more...
+];
+
+export default products;
